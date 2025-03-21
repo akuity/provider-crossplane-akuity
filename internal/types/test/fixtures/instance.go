d553c80d41ff43a2a3db06ba9db7c06d70057db0
Yiwei Gong <imwithye@gmail.com>
2025-03-21 15:13:23 +0800

    fix compiler error

--- internal/types/test/fixtures/instance.go
+++ internal/types/test/fixtures/instance.go
@@ -49,7 +49,7 @@ var (
 
 	AkuityAppsetPolicy = &akuitytypes.AppsetPolicy{
 		Policy:         "policy",
-		OverridePolicy: ptr.To(true),
+		OverridePolicy: true,
 	}
 
 	ArgocdAppsetPolicy = &argocdv1.AppsetPolicy{
@@ -59,7 +59,7 @@ var (
 
 	CrossplaneAppsetPolicy = &crossplanetypes.AppsetPolicy{
 		Policy:         "policy",
-		OverridePolicy: true,
+		OverridePolicy: ptr.To(true),
 	}
 
 	AkuityAppSetDelegate = &akuitytypes.AppSetDelegate{
@@ -81,7 +81,7 @@ var (
 	}
 
 	AkuityImageUpdaterDelegate = &akuitytypes.ImageUpdaterDelegate{
-		ControlPlane: ptr.To(true),
+		ControlPlane: true,
 		ManagedCluster: &akuitytypes.ManagedCluster{
 			ClusterName: "test-cluster",
 		},
@@ -95,14 +95,14 @@ var (
 	}
 
 	CrossplaneImageUpdateDelegate = &crossplanetypes.ImageUpdaterDelegate{
-		ControlPlane: true,
+		ControlPlane: ptr.To(true),
 		ManagedCluster: &crossplanetypes.ManagedCluster{
 			ClusterName: "test-cluster",
 		},
 	}
 
 	AkuityRepoServerDelegate = &akuitytypes.RepoServerDelegate{
-		ControlPlane: ptr.To(true),
+		ControlPlane: true,
 		ManagedCluster: &akuitytypes.ManagedCluster{
 			ClusterName: "test-cluster",
 		},
@@ -116,7 +116,7 @@ var (
 	}
 
 	CrossplaneRepoServerDelegate = &crossplanetypes.RepoServerDelegate{
-		ControlPlane: true,
+		ControlPlane: ptr.To(true),
 		ManagedCluster: &crossplanetypes.ManagedCluster{
 			ClusterName: "test-cluster",
 		},
@@ -189,10 +189,10 @@ var (
 	}
 
 	AkuityClusterCustomization = &akuitytypes.ClusterCustomization{
-		AutoUpgradeDisabled: ptr.To(true),
+		AutoUpgradeDisabled: true,
 		Kustomization:       Kustomization,
-		AppReplication:      ptr.To(true),
-		RedisTunneling:      ptr.To(true),
+		AppReplication:      true,
+		RedisTunneling:      true,
 	}
 
 	ArgocdClusterCustomization = &argocdv1.ClusterCustomization{
@@ -203,30 +203,30 @@ var (
 	}
 
 	CrossplaneClusterCustomization = &crossplanetypes.ClusterCustomization{
-		AutoUpgradeDisabled: true,
+		AutoUpgradeDisabled: ptr.To(true),
 		Kustomization:       KustomizationYAML,
-		AppReplication:      true,
-		RedisTunneling:      true,
+		AppReplication:      ptr.To(true),
+		RedisTunneling:      ptr.To(true),
 	}
 
 	AkuityInstanceSpec = akuitytypes.InstanceSpec{
 		IpAllowList:                     AkuityIpAllowList,
 		Subdomain:                       "example.com",
-		DeclarativeManagementEnabled:    ptr.To(true),
+		DeclarativeManagementEnabled:    true,
 		Extensions:                      AkuityInstallEntryList,
 		ClusterCustomizationDefaults:    AkuityClusterCustomization,
-		ImageUpdaterEnabled:             ptr.To(true),
-		BackendIpAllowListEnabled:       ptr.To(true),
+		ImageUpdaterEnabled:             true,
+		BackendIpAllowListEnabled:       true,
 		RepoServerDelegate:              AkuityRepoServerDelegate,
-		AuditExtensionEnabled:           ptr.To(true),
-		SyncHistoryExtensionEnabled:     ptr.To(true),
+		AuditExtensionEnabled:           true,
+		SyncHistoryExtensionEnabled:     true,
 		ImageUpdaterDelegate:            AkuityImageUpdaterDelegate,
 		AppSetDelegate:                  AkuityAppSetDelegate,
-		AssistantExtensionEnabled:       ptr.To(true),
+		AssistantExtensionEnabled:       true,
 		AppsetPolicy:                    AkuityAppsetPolicy,
 		HostAliases:                     AkuityHostAliasesList,
-		Fqdn:                            ptr.To(""),
-		MultiClusterK8SDashboardEnabled: ptr.To(true),
+		Fqdn:                            "",
+		MultiClusterK8SDashboardEnabled: true,
 	}
 
 	ArgocdInstanceSpec = &argocdv1.InstanceSpec{
@@ -253,16 +253,16 @@ var (
 		ClusterCustomizationDefaults:    CrossplaneClusterCustomization,
 		IpAllowList:                     CrossplaneIpAllowList,
 		Subdomain:                       "example.com",
-		DeclarativeManagementEnabled:    true,
+		DeclarativeManagementEnabled:    ptr.To(true),
 		Extensions:                      CrossplaneInstallEntryList,
-		ImageUpdaterEnabled:             true,
-		BackendIpAllowListEnabled:       true,
+		ImageUpdaterEnabled:             ptr.To(true),
+		BackendIpAllowListEnabled:       ptr.To(true),
 		RepoServerDelegate:              CrossplaneRepoServerDelegate,
-		AuditExtensionEnabled:           true,
-		SyncHistoryExtensionEnabled:     true,
+		AuditExtensionEnabled:           ptr.To(true),
+		SyncHistoryExtensionEnabled:     ptr.To(true),
 		ImageUpdaterDelegate:            CrossplaneImageUpdateDelegate,
 		AppSetDelegate:                  CrossplaneAppSetDelegate,
-		AssistantExtensionEnabled:       true,
+		AssistantExtensionEnabled:       ptr.To(true),
 		AppsetPolicy:                    CrossplaneAppsetPolicy,
 		HostAliases:                     CrossplaneHostAliasesList,
 		MultiClusterK8SDashboardEnabled: &trueValue,
